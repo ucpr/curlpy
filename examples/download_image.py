3a96d6efa6e1c46ddce6db35a2d2770ed9e97454
ucpr <contact@ucpr.dev>
2020-08-26 19:30:50 +0900

    style: run black

--- examples/download_image.py
+++ examples/download_image.py
@@ -1,4 +1,9 @@
 from curlpy import Curlpy
 
 
-req = Curlpy("https://httpbin.org/image/png").H({"accept": "image/png"}).fetch().o("hoge.png", binary=True)
+req = (
+    Curlpy("https://httpbin.org/image/png")
+    .H({"accept": "image/png"})
+    .fetch()
+    .o("hoge.png", binary=True)
+)
